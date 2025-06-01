2240d05890e517b7301a75f1c906492d697ec9c6
Phinehas Osei-Tutu <phinehasoseitutu1@gmail.com>
2025-06-01 16:09:29 +0000

    updated routing

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
       <Header />
       <main className="flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 flex-grow">
         <div className="text-center">
-          <h1 className="text-5xl font-bold text-white">Welcome to our Application!</h1>
+          <h1 className="text-5xl font-bold text-white"> Welcome to our Application!</h1>
           <p className="text-xl mt-4 text-white">We're glad you're here. Enjoy and explore your experience.</p>
           <Button title="Get Started" styles="mt-6 px-6 bg-white text-blue-500 rounded-full font-semibold hover-gray-200 transition" />
         </div>
